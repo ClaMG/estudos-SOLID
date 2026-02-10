c14bc69def3249e9fc09974cf04ada8a787c8e0b
Erick <erick.galdino@gfxconsultoria.com>
2026-02-10 09:05:01 -0300

    correção

--- backend/src/core/entities/entitiesUser.js
+++ backend/src/core/entities/entitiesUser.js
@@ -2,13 +2,20 @@ import {validateEmail} from '../../shared/utils/validators/validatorEmail.js'
 import {validateCPF} from '../../shared/utils/validators/validatorCpf.js'
 
 export class User {
+
     constructor(dto) {
         console.log("Dados recebidos na Entidade:", dto);
+        /** @type {string} */
         this.id = dto.id;
+        /** @type {string} */
         this.user = dto.user;
+        /** @type {string} */
         this.password = dto.password;
+        /** @type {string} */
         this.email = dto.email;
+        /** @type {string} */
         this.cpf = dto.cpf;
+        /** @type {boolean} */
         this.admin = dto.admin || false;
 
         this.validate();
